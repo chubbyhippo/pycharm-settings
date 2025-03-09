0b953074e292b9095a0bf5061d460893718e0ffb
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-09 21:57:28 +0700

    Update install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env sh
 
+# plugins
+cmd=pycharm
+if uname | grep -q "^MINGW"; then
+  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/pycharm.cmd
+fi
+
+$cmd installPlugins \
+IdeaVIM \
+com.jetbrains.kmm \
+com.joshestein.ideavim-quickscope \
+eu.theblob42.idea.whichkey
